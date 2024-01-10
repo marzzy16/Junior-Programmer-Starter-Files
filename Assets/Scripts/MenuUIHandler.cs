a0eebae0b3e23306a748539171db0e2118ef3a77
marzzy16 <114106672+marzzy16@users.noreply.github.com>
2024-01-10 15:16:17 -0600

    Initial commit

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    public ColorPicker ColorPicker;
+
+    public bool UNITY_EDITOR { get; private set; }
+
+    public void NewColorSelected(Color color)
+    {
+        // add code here to handle when a color is selected
+    }
+    
+    private void Start()
+    {
+        ColorPicker.Init();
+        //this will call the NewColorSelected function when the color picker have a color button clicked.
+        ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if (UNITY_EDITOR)
+        {
+           EditorApplication.ExitPlaymode();
+        }
+
+
+        #else
+        { 
+            Application.Quit();
+        }
+#endif 
+
+    }
+
+
+}
